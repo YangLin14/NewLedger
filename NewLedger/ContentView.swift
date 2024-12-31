f379e39ed39b2cabfaec603cc719532f88b18d7d
YangLin14 <fongyu93@gmail.com>
2024-12-30 19:31:11 -0800

    Initial Commit

---
+++ NewLedger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewLedger
+//
+//  Created by Fong Yu Lin on 12/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewLedger/NewLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLedgerApp.swift
+//  NewLedger
+//
+//  Created by Fong Yu Lin on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NewLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
